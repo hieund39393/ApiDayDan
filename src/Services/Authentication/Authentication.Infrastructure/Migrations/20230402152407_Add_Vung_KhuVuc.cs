11082978442e14bf40477c924e7bb0c425c6de8e
Admin <Admin@DESKTOP-2QREFK2>
2023-04-02 22:24:42 +0700

    commit

---
+++ src/Services/Authentication/Authentication.Infrastructure/Migrations/20230402152407_Add_Vung_KhuVuc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authentication.Infrastructure.Migrations
+{
+    public partial class Add_Vung_KhuVuc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DM_KhuVuc",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Id bảng, khóa chính"),
+                    TenKhuVuc = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    GhiChu = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false, comment: "Cờ xóa"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Ngày tạo"),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: true, comment: "Mã người tạo"),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true, comment: "Ngày cập nhật"),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: true, comment: "Mã người cập nhật")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DM_KhuVuc", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DM_Vung",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Id bảng, khóa chính"),
+                    TenVung = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    GhiChu = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false, comment: "Cờ xóa"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Ngày tạo"),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: true, comment: "Mã người tạo"),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true, comment: "Ngày cập nhật"),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", maxLength: 36, nullable: true, comment: "Mã người cập nhật")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DM_Vung", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DM_KhuVuc");
+
+            migrationBuilder.DropTable(
+                name: "DM_Vung");
+        }
+    }
+}
