f8a9cf2de3cf125b352d1a7944eb18350202d091
Tien Anh Do <Tien Anh Do@DESKTOP-75P8005>
2023-04-13 00:30:00 +0700

    update-validate

---
+++ src/Services/Authentication/Authentication.API/Infrastructure/Validations/DonGiaNhanCong/DonGiaVatLieuValidator.cs
@@ -0,0 +1,18 @@
+﻿using Authentication.Application.Commands.DonGiaNhanCongCommand;
+using Authentication.Infrastructure.Properties;
+using FluentValidation;
+
+namespace Authentication.API.Infrastructure.Validations.DonGiaNhanCong
+{
+    public class DonGiaNhanCongValidator : AbstractValidator<CreateDonGiaNhanCongCommand>
+    {
+        public DonGiaNhanCongValidator()
+        {
+            RuleFor(x => x.HeSo).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Nhân công"));
+            RuleFor(x => x.DonGia).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Đơn giá"));
+            RuleFor(x => x.CapBac).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Cấp bậc"));
+            RuleFor(x => x.IdVung).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Vùng"));
+            RuleFor(x => x.IdKhuVuc).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Khu vực"));
+        }
+    }
+}
