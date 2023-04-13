53ee5fb1f13633560292ae95cbfdffffcdfc3dc5
Tien Anh Do <Tien Anh Do@DESKTOP-75P8005>
2023-04-13 23:55:07 +0700

    update-validate

---
+++ src/Services/Authentication/Authentication.API/Infrastructure/Validations/ChiTietBieuGia/ChiTietBieuGiaValidator.cs
@@ -0,0 +1,17 @@
+﻿using Authentication.Application.Commands.ChiTietBieuGiaCommand;
+using Authentication.Application.Model.ChiTietBieuGia;
+using Authentication.Infrastructure.Properties;
+using FluentValidation;
+
+namespace Authentication.API.Infrastructure.Validations.ChiTietBieuGia
+{
+    public class ChiTietBieuGiaValidator : AbstractValidator<ChiTietBieuGiaRequest>
+    {
+        public ChiTietBieuGiaValidator()
+        {
+            RuleFor(x => x.Quy).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Quý"));
+            RuleFor(x => x.Nam).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Năm"));
+            RuleFor(x => x.IDBieuGia).NotEmpty().WithMessage(string.Format(Resources.MSG_REQUIRED_FIELD, "Biểu giá"));
+        }
+    }
+}
