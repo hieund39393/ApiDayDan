2150a3e49e0a7710d76cbb23adfef35f3c4bef89
Hieu.NguyenDuy <Hieu.NguyenDuy@euroland.com>
2023-07-19 16:48:42 +0700

    pl

--- src/Services/Authentication/Authentication.Application/Commands/ChiTietBieuGiaCommand/SyncChiTietBieuGiaCommand.cs
+++ src/Services/Authentication/Authentication.Application/Commands/ChiTietBieuGiaCommand/SyncChiTietBieuGiaCommand.cs
@@ -13,6 +13,9 @@ namespace Authentication.Application.Commands.ChiTietBieuGiaCommand
 {
     public class SyncChiTietBieuGiaCommand : IRequest<bool>
     {
+        public int Nam { get; set; }
+        public int Quy { get; set; }
+        public int LoaiDongBo { get; set; }
     }
     public class SyncChiTietBieuGiaCommandHandler : IRequestHandler<SyncChiTietBieuGiaCommand, bool>
     {
@@ -23,45 +26,96 @@ namespace Authentication.Application.Commands.ChiTietBieuGiaCommand
         }
         public async Task<bool> Handle(SyncChiTietBieuGiaCommand request, CancellationToken cancellationToken)
         {
-            var bieuGiaCu = await _unitOfWork.BieuGiaTongHopRepository.GetQuery(x=>x.TinhTrang == TinhTrangEnum.DaDuyet.GetHashCode()).OrderByDescending(x => x.Nam).ThenByDescending(x => x.Quy).AsNoTracking().FirstOrDefaultAsync();
-            var chiTietBieuGiaCu = await _unitOfWork.ChiTietBieuGiaRepository.GetQuery(x => x.Nam == bieuGiaCu.Nam && x.Quy == bieuGiaCu.Quy).AsNoTracking().ToListAsync();
-            var listChiTietBieuGia = new List<ChiTietBieuGia>();
-            foreach (var item in chiTietBieuGiaCu)
+            var bieuGiaCu = await _unitOfWork.BieuGiaTongHopRepository.GetQuery(x => x.TinhTrang == TinhTrangEnum.DaDuyet.GetHashCode()).OrderByDescending(x => x.Nam).ThenByDescending(x => x.Quy).AsNoTracking().FirstOrDefaultAsync();
+
+            if (request.LoaiDongBo == 1)
             {
-                item.Id = Guid.NewGuid();
-                if (item.Quy == 4)
+                var chiTietBieuGiaCu = await _unitOfWork.ChiTietBieuGiaRepository.GetQuery(x => x.Nam == bieuGiaCu.Nam && x.Quy == bieuGiaCu.Quy).AsNoTracking().ToListAsync();
+                var listChiTietBieuGia = new List<ChiTietBieuGia>();
+                foreach (var item in chiTietBieuGiaCu)
                 {
-                    item.Quy = 1;
-                    item.Nam += 1;
+                    item.Id = Guid.NewGuid();
+                    if (item.Quy == 4)
+                    {
+                        item.Quy = 1;
+                        item.Nam += 1;
+                    }
+                    else
+                    {
+                        item.Quy += 1;
+                    }
+                    var checkExist = await _unitOfWork.ChiTietBieuGiaRepository
+                        .FindOneAsync(x => x.IDBieuGia == item.IDBieuGia && x.IDCongViec == item.IDCongViec && x.Nam == item.Nam && x.Quy == item.Quy);
+                    if (checkExist == null)
+                    {
+                        listChiTietBieuGia.Add(item);
+
+                    }
+
+                }
+                _unitOfWork.ChiTietBieuGiaRepository.AddRange(listChiTietBieuGia);
+
+                bieuGiaCu.Id = Guid.NewGuid();
+                bieuGiaCu.TinhTrang = TinhTrangEnum.TaoMoi.GetHashCode();
+                if (bieuGiaCu.Quy == 4)
+                {
+                    bieuGiaCu.Quy = 1;
+                    bieuGiaCu.Nam += 1;
                 }
                 else
                 {
-                    item.Quy += 1;
+                    bieuGiaCu.Quy += 1;
                 }
-                var checkExist = await _unitOfWork.ChiTietBieuGiaRepository
-                    .FindOneAsync(x => x.IDBieuGia == item.IDBieuGia && x.IDCongViec == item.IDCongViec && x.Nam == item.Nam && x.Quy == item.Quy);
-                if (checkExist == null)
+
+                _unitOfWork.BieuGiaTongHopRepository.Add(bieuGiaCu);
+            }
+            else if (request.LoaiDongBo == 2)
+            {
+                if (bieuGiaCu.Nam != request.Nam || bieuGiaCu.Quy != request.Quy)
                 {
-                    listChiTietBieuGia.Add(item);
-
+                    throw new EvnException($"Quý {request.Quy} năm {request.Nam} chưa có dữ liệu vui lòng đồng bộ dữ liệu của quý trước khi thực hiện thay đổi đơn giá");
                 }
 
-            }
-            _unitOfWork.ChiTietBieuGiaRepository.AddRange(listChiTietBieuGia);
+                var chiTietBieuGiaCu = await _unitOfWork.ChiTietBieuGiaRepository.GetQuery(x => x.Nam == bieuGiaCu.Nam && x.Quy == bieuGiaCu.Quy).AsNoTracking().ToListAsync();
+                var listIdBieuGiaCu = chiTietBieuGiaCu.Select(x => x.IDBieuGia).Distinct().ToList();
+                var listBieuGiaCongViec = await _unitOfWork.BieuGiaCongViecRepository.GetQuery(x => listIdBieuGiaCu.Contains(x.IdBieuGia))
+                    .Include(x => x.DM_CongViec).AsNoTracking().ToListAsync();
 
-            bieuGiaCu.Id = Guid.NewGuid();
-            bieuGiaCu.TinhTrang = TinhTrangEnum.TaoMoi.GetHashCode();
-            if (bieuGiaCu.Quy == 4)
-            {
-                bieuGiaCu.Quy = 1;
-                bieuGiaCu.Nam += 1;
-            }
-            else
-            {
-                bieuGiaCu.Quy += 1;
-            }
+                var listDonGiaCap = await _unitOfWork.GiaCapRepository.GetQuery().GroupBy(x => x.IdLoaiCap).Select(x => x.OrderByDescending(y => y.CreatedDate).First())
+                    .Include(z => z.DM_LoaiCap).AsNoTracking().ToListAsync();
 
-            _unitOfWork.BieuGiaTongHopRepository.Add(bieuGiaCu);
+                var listDonGiaChietTinh = await _unitOfWork.DonGiaChietTinhRepository.GetQuery()
+                    .GroupBy(x => x.IdCongViec).Select(x => x.OrderByDescending(y => y.CreatedDate).First()).Include(z => z.DM_CongViec).AsNoTracking().ToListAsync();
+
+                var listDonGiaVatLieu = await _unitOfWork.DonGiaVatLieuRepository.GetQuery()
+                    .GroupBy(x => x.IdVatLieu).Select(x => x.OrderByDescending(y => y.CreatedDate).First()).Include(z => z.DM_VatLieu).AsNoTracking().ToListAsync();
+
+                var listChiTietBieuGia = new List<ChiTietBieuGia>();
+
+                foreach (var item in chiTietBieuGiaCu)
+                {
+                    var cap = listBieuGiaCongViec.Where(x => x.IdCongViec == item.IDCongViec && x.IdBieuGia == item.IDBieuGia).FirstOrDefault();
+                    if (cap.CongViecChinh)
+                    {
+                        var giaCap = listDonGiaCap.Where(x => x.DM_LoaiCap.MaLoaiCap.Trim() == cap.DM_CongViec.MaCongViec.Trim()).FirstOrDefault()?.DonGia;
+                        item.DonGia_VL = giaCap.Value;
+                    }
+                    else if (!string.IsNullOrEmpty(item.DM_CongViec.MaCongViec) && item.DM_CongViec.MaCongViec.ToUpper().StartsWith("D"))
+                    {
+                        var donGiaCT = listDonGiaChietTinh.Where(x => x.IdCongViec == item.IDCongViec).FirstOrDefault();
+                        item.DonGia_VL = donGiaCT.DonGiaVatLieu ?? item.DonGia_VL;
+                        item.DonGia_NC = donGiaCT.DonGiaNhanCong ?? item.DonGia_NC;
+                        item.DonGia_MTC = donGiaCT.DonGiaMTC ?? item.DonGia_MTC;
+                    }
+                    else
+                    {
+                        var vatLieu = listDonGiaVatLieu.Where(x => x.IdVatLieu == item.IDCongViec).FirstOrDefault();
+                        item.DonGia_VL = vatLieu?.DonGia ?? item.DonGia_VL;
+                    }
+
+                    _unitOfWork.ChiTietBieuGiaRepository.Update(item);
+                }
+            }
 
             await _unitOfWork.SaveChangesAsync();
             return true;
